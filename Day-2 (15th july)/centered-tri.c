38d588227484289e5f9153e447076e22603b64a2
AnjaliGhildiyal5 <ghildiyalanjali1985@gmail.com>
2026-07-15 23:54:29 +0530

    Add files via upload

---
+++ Day-2 (15th july)/centered-tri.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// it is a daimond actually
+
+int main() {
+    for (int k = 1; k<=5; k++) {
+        for (int i = 1; i<=5-k; i++) {
+            printf(" ");
+        }
+        for (int j = 1; j<=2*k-1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (int k = 5; k>=0; k--) {
+        for (int i = 1; i<=5-k; i++) {
+            printf(" ");
+        }
+        for (int j = 2*k-1; j>=1; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
