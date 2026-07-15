590505a7bdc71b2476437b171fd76c94d2432ab6
AnjaliGhildiyal5 <ghildiyalanjali1985@gmail.com>
2026-07-16 00:01:21 +0530

    Update centered-tri.c

--- Day-2 (15th july)/centered-tri.c
+++ Day-2 (15th july)/centered-tri.c
@@ -14,10 +14,10 @@ int main() {
     }
 
     for (int k = 5; k>=0; k--) {
-        for (int i = 1; i<=5-k; i++) {
+        for (int i = 1; i<=6-k; i++) {
             printf(" ");
         }
-        for (int j = 2*k-1; j>=1; j--) {
+        for (int j = 2*k-2; j>=2; j--) {
             printf("*");
         }
         printf("\n");
